89d57a3041419771803cf9257eb8b67b6d167c3c
Solange <sol.cer.87@gmail.com>
2018-11-25 20:09:46 +0000

    Task2 completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 "use-strict";
+
 const theForm = document.getElementById("the-form");
 const theDate = document.getElementById("date");
 const getDate = document.querySelector('input[type="date"]');
 
 getDate.addEventListener("change", showDate);
 
+let originalValue = theDate.innerText;
+
+let today = new Date();
+parseDate(today);
+
 function showDate(){
   let d = new Date(getDate.value);
-  let originalValue = theDate.innerText;
-  theDate.innerText = "${originalValue} ${d.toDateString()}";
+  parseDate(d);
+}
+
+function parseDate(date){
+  theDate.innerText = `${originalValue} ${date.toDateString()}`;
 }
 
