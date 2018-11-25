9788f3f74afb063162c3085a72c49c8c0029b192
Solange <sol.cer.87@gmail.com>
2018-11-25 20:53:05 +0000

    Task3 completed

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,33 @@ const theForm = document.getElementById("the-form");
 const theDate = document.getElementById("date");
 const getDate = document.querySelector('input[type="date"]');
 
+const theColour = document.getElementById("colour");
+const theValue = document.getElementById("the-value");
+
+
 getDate.addEventListener("change", showDate);
+theForm.addEventListener("input", setColor);
 
 let originalValue = theDate.innerText;
 
 let today = new Date();
 parseDate(today);
+setColor();
 
-function showDate(){
+function showDate()
+{
   let d = new Date(getDate.value);
   parseDate(d);
 }
 
-function parseDate(date){
+function parseDate(date)
+{
   theDate.innerText = `${originalValue} ${date.toDateString()}`;
 }
+
+function setColor()
+{
+  theValue.style.color = theColour.value;
+}
+
 
